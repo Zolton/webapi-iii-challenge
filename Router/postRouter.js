390075f4c4fbef0bbb5bd43955d464332f3c6580
Zolton <gmzesq@gmail.com>
2019-08-07 15:49:06 -0400

    Basic CRUD and GET/POST for posts working

--- Router/postRouter.js
+++ Router/postRouter.js
@@ -1,9 +1,35 @@
 const router = require("express").Router();
 
 let posts = require("../posts/postDb");
+let users = require("../users/userDb");
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to router" });
+  posts.get().then(posts => {
+    res.status(200).json(posts);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  id = req.params.id;
+  users
+    .getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json("There was a 500 status error");
+    });
+});
+
+router.post("/", (req, res) => {
+  newPost = req.body;
+  posts.insert(newPost)
+  .then(post => {
+    res.status(200).json(newPost);
+  })
+  .catch(error => {
+    res.status(500).json("There was a 500 status error");
+  });
 });
 
 module.exports = router;
